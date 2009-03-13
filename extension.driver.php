f49939a904aeb8da79df2cc8bc29d2abe0ae07c3
Nils Hörrmann <post@nilshoerrmann.de>
2009-03-13 11:29:36 +0100

    updated version number and readme

--- extension.driver.php
+++ extension.driver.php
@@ -6,8 +6,8 @@
 		
 			return array(
 				'name' => 'Field: Mediathek',
-				'version' => '1.1.2',
-				'release-date' => '2009-03-05',
+				'version' => '1.1.3',
+				'release-date' => '2009-03-13',
 				'author' => array(
 					'name' => 'Nils Hörrmann',
 					'website' => 'http://www.nilshoerrmann.de',
