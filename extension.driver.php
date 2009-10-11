9652ba8c2e17d7dd69684e2fa885672578735f89
Rainer Borene <rainerborene@gmail.com>
2009-10-12 00:23:30 +0800

    Improved __appendAssets function.

--- extension.driver.php
+++ extension.driver.php
@@ -1,5 +1,7 @@
 <?php
 
+	require_once(TOOLKIT . '/class.sectionmanager.php');
+
 	Class extension_mediathek extends Extension {
 	
 		/**
@@ -55,17 +57,29 @@
 		 */
 
 		public function __appendAssets($context) {
+			$callback = Administration::instance()->getPageCallback();
+
 			// append javascript for field settings pane
-			if (Administration::instance()->Page instanceof contentBlueprintsSections) {
+			if ($callback['driver'] == 'blueprintssections' && is_array($callback['context'])){
 				Administration::instance()->Page->addScriptToHead(URL . '/extensions/mediathek/assets/section.js', 100, false);
 			}
+
 			// append styles and javascript for mediasection display
-			if (Administration::instance()->Page instanceof contentPublish) {
-				Administration::instance()->Page->addScriptToHead(URL . '/extensions/mediathek/assets/mediasection.js', 100, false);
-				Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/mediathek/assets/mediasection.css', 'screen', 101, false);
+			if ($callback['driver'] == 'publish' && $callback['context']['page'] == 'edit'){
+				$sectionManager = new SectionManager($this->_Parent);
+				$section_handle = Administration::instance()->Page->_context['section_handle'];
+				$section_id = $sectionManager->fetchIDFromHandle($section_handle);
+
+				// verifies if current section has a mediathek field
+				$mediathek = Administration::instance()->Database->fetchCol("id", "SELECT id FROM tbl_fields WHERE parent_section = {$section_id} AND type = 'mediathek'");
+
+				if ($mediathek){
+					Administration::instance()->Page->addScriptToHead(URL . '/extensions/mediathek/assets/mediasection.js', 100, false);
+					Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/mediathek/assets/mediasection.css', 'screen', 101, false);
+				}
 			}
 		}
-			
+
 		/**
 		 * Saves sort order of the field
 		 *
